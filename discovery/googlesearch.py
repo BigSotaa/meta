d24a7e32ec8cc251336c51d678479786adb078ea
oaoouo <oaoouo@yeah.net>
2017-12-06 18:22:01 +0800

    fix google search return 301 or 302 bug

--- discovery/googlesearch.py
+++ discovery/googlesearch.py
@@ -5,20 +5,20 @@ import re
 import time
 
 class search_google:
-	def __init__(self,word,limit,start,filetype):
+	def __init__(self,word,limit,start,filetype, area):
 		self.word=word
 		self.results=""
 		self.totalresults=""
 		self.filetype=filetype
-		self.server="www.google.com"
-		self.hostname="www.google.com"
+		self.server="google.com" + "." + area
+		self.hostname="www.google.com" + "." + area
 		self.userAgent="(Mozilla/5.0 (Windows; U; Windows NT 6.0;en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"
 		self.quantity="100"
 		self.limit=limit
 		self.counter=start
 		
 	def do_search_files(self):
-		h = httplib.HTTP(self.server)
+		h = httplib.HTTPS(self.server)
 		h.putrequest('GET', "/search?num="+self.quantity+"&start=" + str(self.counter) + "&hl=en&meta=&q=filetype:"+self.filetype+"%20site:" + self.word)
 		h.putheader('Host', self.hostname)
 		h.putheader('User-agent', self.userAgent)	
